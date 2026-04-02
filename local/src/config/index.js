eb0ddd0ea433f1447fab315f82c19f1dddcd444e
Gleb Neverov <hellhands@yandex.ru>
2026-04-02 18:09:02 +0700

    Initial commit

---
+++ local/src/config/index.js
@@ -0,0 +1,6 @@
+export const contacts = {
+    phone: '+7 (993) 206-63-68',
+    phoneHref: '+79932066368',
+    email: 'podoexpert@mail.ru',
+    address: 'Москва, ул. Шаболовка д.23 кор.5 (1 этаж)',
+};

c70d0df444422f0217449409cfffce35b62453dd
Gleb Neverov <hellhands@yandex.ru>
2026-04-08 16:54:11 +0700

    Fix(Программирование) - Header - Footer

--- prod/index.php
+++ prod/index.php
@@ -1,6 +1,8 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Новости банка");
+$APPLICATION->SetPageProperty("keywords", "Курсы медицинского педикюра в Санкт-Петербурге");
+$APPLICATION->SetPageProperty("description", "Курсы медицинского педикюра в Санкт-Петербурге. Обучение и курсы аттестации по подологии и маникюру. Работа с вросшим ногтем. Диплом. Стажировка. Помощь в трудоустройстве");
+$APPLICATION->SetTitle("Школа «AEK-School»");
 ?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
